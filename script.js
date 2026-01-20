616892cf4ea64ee01e99d4b8406044a2d7322810
sababg <beigisaba@gmail.com>
2026-01-19 22:15:51 -0500

    Add the final validation in submit form

--- script.js
+++ script.js
@@ -8,17 +8,21 @@ let emailError = document.getElementById("emailError");
 let passwordError = document.getElementById("passwordError");
 let confirmPasswordError = document.getElementById("confirmPasswordError");
 
+// Load saved username
 window.addEventListener("load", () => {
   if (localStorage.getItem("username")) {
     username.value = localStorage.getItem("username");
   }
 });
 
+//Real-time validation
 const validateUsername = (input) => {
   if (input.validity.valueMissing) {
     usernameError.textContent = "This field is required.";
+    return false;
   } else {
     usernameError.textContent = "";
+    return true;
   }
 };
 
@@ -31,10 +35,13 @@ const validateEmail = (input) => {
   input.setCustomValidity("");
   if (input.validity.typeMismatch) {
     emailError.textContent = "Please enter a valid email address.";
+    return false;
   } else if (input.validity.valueMissing) {
     emailError.textContent = "This field is required.";
+    return false;
   } else {
     emailError.textContent = "";
+    return true;
   }
 };
 
@@ -43,39 +50,53 @@ email.addEventListener("input", (e) => {
   validateEmail(input);
 });
 
-password.addEventListener("input", (e) => {
-  const input = e.target;
+const validatePassword = (input) => {
   const hasUpperCase = /[A-Z]/;
   const hasLowerCase = /[a-z]/;
   const hasNumber = /\d/;
   if (input.validity.valueMissing) {
     passwordError.textContent = "This field is required.";
+    return false;
   } else if (input.value.length < 8) {
     passwordError.textContent = "Password must be at least 8 characters long.";
+    return false;
   } else if (!hasUpperCase.test(input.value)) {
     passwordError.textContent =
       "Password must include at least an uppercase character.";
+    return false;
   } else if (!hasLowerCase.test(input.value)) {
     passwordError.textContent =
       "Password must include at least an lowercase character.";
+    return false;
   } else if (!hasNumber.test(input.value)) {
     passwordError.textContent = "Password must include at least a number.";
+    return false;
   } else {
     passwordError.textContent = "";
+    return true;
   }
+};
+
+password.addEventListener("input", (e) => {
+  const input = e.target;
+  validatePassword(input);
 });
 
 const validateConfirmPassword = (input) => {
   if (input.validity.valueMissing) {
     confirmPasswordError.textContent = "This field is required.";
+    return false;
   } else if (input.value.length < 8) {
     confirmPasswordError.textContent =
       "Password must be at least 8 characters long.";
+    return false;
   } else if (input.value !== password.value) {
     confirmPasswordError.textContent =
       "Confirm Password is not the same as the password";
+    return false;
   } else {
     confirmPasswordError.textContent = "";
+    return true;
   }
 };
 
@@ -84,13 +105,26 @@ confirmPassword.addEventListener("input", (e) => {
   validateConfirmPassword(input);
 });
 
-registrationForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+//Form submission
+registrationForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  const isUsernameValid = validateUsername(registrationForm.elements.username);
-  const isEmailValid = validateEmail(registrationForm.elements.email);
-  const isPasswordValid = validatePassword();
-  const isConfirmPasswordValid = validateConfirmPassword();
+  const isUsernameValid = validateUsername(username); // use the  document.getElementById
+  // **********or we can use The document.getElementById, Means we can use username instead of registrationForm.elements.username**************
+  const isEmailValid = validateEmail(registrationForm.elements.email); // used the name of the input, and it's not really safe to use
+  const isPasswordValid = validatePassword(registrationForm.elements.password);
+  const isConfirmPasswordValid = validateConfirmPassword(
+    registrationForm.elements.confirmPassword,
+  );
 
-  alert("Registration successful");
+  if (
+    isUsernameValid &&
+    isEmailValid &&
+    isPasswordValid &&
+    isConfirmPasswordValid
+  ) {
+    alert("Registration successful");
+    localStorage.setItem("username", username.value);
+    registrationForm.reset();
+  }
 });
